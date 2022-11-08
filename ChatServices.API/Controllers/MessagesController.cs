5d7944f8fa7f233fbe2f48639dec03b932b3e6e2
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2022-11-08 13:38:49 +0700

    update chat

--- ChatServices.API/Controllers/MessagesController.cs
+++ ChatServices.API/Controllers/MessagesController.cs
@@ -1,5 +1,6 @@
 ﻿using ChatServices.API.DTOs;
 using ChatServices.API.Models;
+using ChatServices.API.Utils;
 using DataLayer.Domain;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -74,12 +75,69 @@ namespace ChatServices.API.Controllers
 
         }
 
+        [HttpPost]
+        [Authorize(Roles = nameof(UserType.Receptionist))]
+        public IActionResult RecToPat(RecToPatMessage request)
+        {
+            try
+            {
+                Patient toPatient = _servicesManager.DbContext.Patients
+                    .Include(pat => pat.BaseUser)
+                    .FirstOrDefault(pat => pat.Id == request.PatientId);
+
+                if(toPatient == null)
+                {
+                    return BadRequest("Truyền cái PatientId đúng chưa? Patient not found!");
+                }
+
+                string loggedUserName = User.Identity.Name;
+                BaseUser loggedUser = _servicesManager.DbContext
+                    .Users.FirstOrDefault(u => u.UserName == loggedUserName);
+
+                ChatMessage message = _servicesManager.AutoMapper.Map<ChatMessage>(request);
+                message.FromId = loggedUser.Id;
+
+                message.ToId = request.PatientId;
+
+                _servicesManager.DbContext.Add(message);
+                _servicesManager.DbContext.SaveChanges();
+
+                ChatMessageDTO messageDTO = _servicesManager.AutoMapper.Map<ChatMessageDTO>(message);
+
+                // Push event
+                string[] chanels = _servicesManager.DbContext.Users
+                    .Where(user => user.Id == toPatient.Id)
+                    .Select(user => user.PusherChannel).ToArray();
+
+                Task pushEventTask = _servicesManager.PusherServices
+                    .PushTo(chanels, "Chat-RecToPat", messageDTO, result =>
+                    {
+                        Console.WriteLine("Chat-PatToRec Done at: " + DateTime.Now);
+                        Console.WriteLine("Data: " + JsonConvert.SerializeObject(messageDTO, Formatting.Indented));
+                        Console.WriteLine("To Chanels: ");
+                        foreach (string chanel in chanels)
+                        {
+                            Console.WriteLine(chanel);
+                        }
+                    });
+
+                return Ok(messageDTO);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+        }
+
         [HttpGet]
         [Authorize(Roles = nameof(UserType.Receptionist))]
         public IActionResult ListUsersHasMessage()
         {
             try
             {
+
                 var queries = _servicesManager.DbContext.ChatMessages
                     .Include(message => message.FromUser)
                     .Where(message => message.FromUser.Type == UserType.Patient);
@@ -103,9 +161,70 @@ namespace ChatServices.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListMessagesInConversationOfPatient()
+        [Authorize(Roles = nameof(UserType.Patient))]
+        public IActionResult ListMessagesInConversationOfPatient(int page = 1)
         {
-            return Ok();
+            try
+            {
+                string loggedUserName = User.Identity.Name;
+                BaseUser loggedUser = _servicesManager.DbContext
+                    .Users.FirstOrDefault(u => u.UserName == loggedUserName);
+
+                var queries = _servicesManager.DbContext.ChatMessages
+                    .Include(message => message.FromUser)
+                    .Where(message => message.FromUser.Id == loggedUser.Id || message.ToUser.Id == loggedUser.Id)
+                    .OrderByDescending(message => message.TimeCreated);
+
+                var paginated = new Paginated<ChatMessage>(queries, page);
+
+                ChatMessageDTO[] datasetDTO = _servicesManager.AutoMapper
+                    .Map<ChatMessageDTO[]>(paginated.Items.ToArray());
+
+                return Ok(new
+                {
+                    page = page,
+                    per_page = paginated.PageSize,
+                    total = paginated.QueryCount,
+                    total_pages = paginated.PageCount,
+                    data = datasetDTO,
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{patientId}")]
+        [Authorize(Roles = nameof(UserType.Receptionist))]
+        public IActionResult ListMessagesInConversationOfReception(string patientId, int page = 1)
+        {
+            try
+            {
+                
+                var queries = _servicesManager.DbContext.ChatMessages
+                    .Include(message => message.FromUser)
+                    .Where(message => message.FromUser.Id == patientId || message.ToUser.Id == patientId)
+                    .OrderByDescending(message => message.TimeCreated);
+
+                var paginated = new Paginated<ChatMessage>(queries, page);
+
+                ChatMessageDTO[] datasetDTO = _servicesManager.AutoMapper
+                    .Map<ChatMessageDTO[]>(paginated.Items.ToArray());
+
+                return Ok(new
+                {
+                    page = page,
+                    per_page = paginated.PageSize,
+                    total = paginated.QueryCount,
+                    total_pages = paginated.PageCount,
+                    data = datasetDTO,
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
     }
