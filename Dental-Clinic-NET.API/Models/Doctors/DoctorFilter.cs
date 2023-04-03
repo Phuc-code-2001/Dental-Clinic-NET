f74b47707c2b77c8780364dd9824e1ec133aed10
phuchtce150394 <phuchtce150394@fpt.edu.vn>
2023-04-03 20:12:12 +0700

    Update Doctor

---
+++ Dental-Clinic-NET.API/Models/Doctors/DoctorFilter.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer.Domain;
+using Dental_Clinic_NET.API.Utils;
+using System.Linq;
+
+namespace Dental_Clinic_NET.API.Models.Doctors
+{
+    public class DoctorFilter : PageFilter
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public string Major { get; set; }
+
+        public IQueryable<Doctor> GetFilteredQuery(IQueryable<Doctor> source)
+        {
+            return source.Where(x =>
+                (string.IsNullOrWhiteSpace(UserName) || x.BaseUser.UserName.Contains(UserName)) &&
+                (string.IsNullOrWhiteSpace(Email) || x.BaseUser.Email.Contains(Email)) &&
+                (string.IsNullOrWhiteSpace(PhoneNumber) || x.BaseUser.PhoneNumber.Contains(PhoneNumber)) &&
+                (string.IsNullOrWhiteSpace(Major) || x.Major.Contains(Major))
+            );
+        }
+    }
+}
